485b03462f6568aa3c4482052d9821539ddb9978
elyashar <15066886+elyashar@users.noreply.github.com>
2024-01-17 23:17:20 +0300

    implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -28,6 +28,34 @@ btnNavEl.addEventListener("click", function(){
   headerEl.classList.toggle("nav-open");
 });
 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    
+    // Scroll back to top
+    if (href ==="#")
+    window.scrollTo({
+  top: 0,
+  behavior: "smooth",
+  });
+
+  // Scroll to other links
+  if (href !== "#" && href.startsWith("#")) {
+const sectionEl = document.querySelector(href);
+sectionEl.scrollIntoView({ behavior: "smooth"});
+  }
+
+  // Close mobile navigation
+if (link.classList.contains("main-nav-link"))
+headerEl.classList.toggle("nav-open");
+   });
+ });
 
 
 ///////////////////////////////////////////////////////////
